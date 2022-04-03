d1bfa13185a85b3d7da21b63dcaddf33db6b918d
itsamareshv <95462958+itsamareshv@users.noreply.github.com>
2022-04-03 17:34:13 +0530

    Update settings.py

--- IOT/settings.py
+++ IOT/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-riyy*^cssc5v$+^bh5rezm1+8dcgy(6c_nm-5d=+5(msdab-b*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','iotbyav.herokuapp.com']
 
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
