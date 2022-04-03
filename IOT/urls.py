3cd84a1a95068d9b338eb86e8e98f44b29242a45
itsamareshv <itsamareshv@gmail.com>
2022-04-03 09:42:31 +0530

    second comit

--- IOT/urls.py
+++ IOT/urls.py
@@ -31,4 +31,6 @@ urlpatterns = [
      path('login',login),
      path('loggedin',loggedin),
      path('loginpage',loginpage),
+     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
